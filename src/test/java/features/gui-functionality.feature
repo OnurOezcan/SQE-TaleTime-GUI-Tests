d7b7480a8575090cee843d7f2d2cbce177b036df
Cedric <cedric.c.schreiner@gmail.com>
2020-03-06 22:42:07 +0100

    - Bug fixes - new scenario

--- src/test/java/features/gui-functionality.feature
+++ src/test/java/features/gui-functionality.feature
@@ -2,68 +2,78 @@
 Feature: This .ferature file checks the functionality of the graphical user interface of the TaleTime application.
 
 
-#  Scenario: This scenario is used to check the availability of the Web site.
-#    Given is the URL "http://localhost:8100".
-#    When the URL is entered in the browser,
-#    Then the TaleTime startpage should appear.
-#
-#  Scenario: This scenario checks if the user can navigate from the start page to the Registration-Form
-#    Given is the URL "http://localhost:8100".
-#    When the user clicks on Registration-Button
-#    Then the Registration-Form should open
+  Scenario: This scenario is used to check the availability of the Web site.
+    Given is the URL "http://localhost:8100".
+    When the URL is entered in the browser,
+    Then the TaleTime startpage should appear.
 
-#  Scenario: Check if an account can be created
-#    Given are the following values:
-#      | Designation: | Value:                |
-#      | mat-input-2  | Tester                |
-#      | mat-input-3  | Tester@htwsaarTest.de |
-#      | mat-input-4  | 123456789             |
-#      | mat-input-5  | 123456789             |
-#    When The button Create was clicked with these values
-#    Then a new user should exist.
+  Scenario: This scenario checks if the user can navigate from the start page to the Registration-Form
+    Given is the URL "http://localhost:8100".
+    When the user clicks on Registration-Button
+    Then the Registration-Form should open
 
-#  Scenario: Check if a existing User can Login.
-#    Given a user has an registered account
-#    And is on the start page
-#    When User logs in
-#    Then he should logged in
+  Scenario: Check if an account can be created
+    Given are the following values:
+      | Designation: | Value:                |
+      | mat-input-2  | Tester                |
+      | mat-input-3  | Tester@htwsaarTest.de |
+      | mat-input-4  | 123456789             |
+      | mat-input-5  | 123456789             |
+    When The button Create was clicked with these values
+    Then a new user should exist.
 
-#  Scenario: Check if a new profile can be created within an account.
-#    Given  a logged-in user
-#    When a new profile called "Test Child" is created
-#    Then "1" profile/s should be available
+  Scenario: Check if a existing User can Login.
+    Given a user has an registered account
+    And is on the start page
+    When User logs in
+    Then he should logged in
 
-#  Scenario: Check if User can add multiple profiles.
-#    Given a logged-in user
-#    And a existing profile
-#    When a new profile called "Second Child" is created
-#    Then "2" profile/s should be available
+  Scenario: Check if a new profile can be created within an account.
+    Given  a logged-in user
+    When a new profile called "Test Child" is created
+    Then "1" profile/s should be available
+
+  Scenario: Check if User can add multiple profiles.
+    Given a logged-in user
+    And a existing profile
+    When a new profile called "Second Child" is created
+    Then "2" profile/s should be available
 
   Scenario: Testing if user can Log out
     Given a logged-in user
     And a existing profile
     And a selected profile
-    When user types in his pin under "my Account"
+    When user navigates to user account page
     And clicks on Logout in the next page
     Then the TaleTime startpage should appear.
 
-#  Scenario: Check if User can add new Stories
-#   # Given a logged-in user
-#    When User adds new Storie Titles
-#    Then the new Stories should be available in the "story-menu" section
-#
+  Scenario: Check if User can add new Stories
+    Given a logged-in user
+    And a existing profile
+    And a selected profile
+    When User adds new Story Title
+    Then the new Stories should be available in the "story-menu" section
+
 #  Scenario: Testing the possibility to change the font size
-#   # Given a logged-in user
+#    Given a logged-in user
+#    And a existing profile
+#    And a selected profile
+#    And a added story
 #    When User changes Font-Size to 14px
 #    Then Font size is 14px
-#
-#  Scenario: Testing if user can switch the selected language
-#   # Given a logged-in user
-#    When User changes the Language from "de" to "en"
-#    Then the Application should be in English
-#
-#  Scenario: Testing if user can delete his account
-#   # Given a logged-in user
-#    When user typed in hin pin under "myAccount"
-#    And clicks on delete account
-#    Then he should not able to login again
+
+  Scenario: Testing if user can switch the selected language
+    Given a logged-in user
+    And a existing profile
+    And a selected profile
+    And a added story
+    When User changes the Language from "de" to "en"
+    Then the Application should be in English
+
+  Scenario: Testing if user can delete his account
+    Given a logged-in user
+    And a existing profile
+    And a selected profile
+    When user navigates to user account page
+    And clicks on delete account
+    Then he should not able to login again
--- src/test/java/steps/guiFunctionalitySteps.java
+++ src/test/java/steps/guiFunctionalitySteps.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.opera.OperaDriver;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,7 +21,7 @@ import static hilfsmethoden.hilfsmethoden.*;
 public class guiFunctionalitySteps {
 
     private WebDriver driver = new ChromeDriver();
-    private static String URL = "http://localhost:8100";
+    private static String URL = "http://localhost:8100/";
 
     //switch if you have opera
     //private WebDriver driver = new OperaDriver();
@@ -30,32 +29,31 @@ public class guiFunctionalitySteps {
     // ------------------------------ CUCUMBER GLUE CODE ----------------------------------
 
     @Given("^is the URL \"([^\"]*)\"\\.$")
-    public void isTheURL(String arg1) throws Throwable {
+    public void isTheURL(String arg1) {
         openWebsite(driver, arg1);
         assert true;
     }
 
     @When("^the URL is entered in the browser,$")
-    public void theURLIsEnteredInTheBrowser() throws Throwable {
+    public void theURLIsEnteredInTheBrowser() {
         assert true;
     }
 
     @Then("^the TaleTime startpage should appear\\.$")
-    public void theTaleTimeStartpageShouldAppear() throws Throwable {
-        //waitForObject(driver, "startPageHeading", true);
+    public void theTaleTimeStartpageShouldAppear() {
         waitForDom(driver);
-        assert checkIfObjectExistis(driver, "startPageHeading");
+        Assert.assertEquals(URL, driver.getCurrentUrl());
         driver.quit();
     }
 
     @When("^the user clicks on Registration-Button$")
-    public void theUserClicksOnRegistrationButton() throws Throwable {
+    public void theUserClicksOnRegistrationButton() {
         clickOnObject(driver, "createAccountButton", true);
         assert true;
     }
 
     @Then("^the Registration-Form should open$")
-    public void theRegistrationFormShouldOpen() throws Throwable {
+    public void theRegistrationFormShouldOpen() {
         waitForDom(driver);
         assert checkIfObjectExistis(driver, "createAccountButton");
         driver.quit();
@@ -76,27 +74,27 @@ public class guiFunctionalitySteps {
     }
 
     @When("^The button Create was clicked with these values$")
-    public void theButtonCreateWasClicked() throws Throwable {
+    public void theButtonCreateWasClicked() {
         clickOnObject(driver, "registerAndCreateAccount", true);
         assert true;
     }
 
     @Then("^a new user should exist\\.$")
-    public void aNewUserShouldExist() throws Throwable {
+    public void aNewUserShouldExist() {
         checkIfObjectExistis(driver, "createUserHeader");
-        assert driver.getCurrentUrl().equals("http://localhost:8100/select-user-profile");
+        assert driver.getCurrentUrl().equals( URL + "select-user-profile");
         driver.quit();
     }
 
     @Given("^a logged-in user$")
-    public void aLoggedInUser() throws Throwable {
+    public void aLoggedInUser() {
 //        gotToRegistrationPage(driver);
         loginUser(driver, "Tester@htwsaarTest.de", "123456789");
         assert true;
     }
 
     @When("^a new profile called \"([^\"]*)\" is created$")
-    public void aNewProfileCalledIsCreated(String name) throws Throwable {
+    public void aNewProfileCalledIsCreated(String name) {
         //actually don't work
         waitForObject(driver, "createProfileButton", true);
         clickOnObject(driver, "createProfileButton", true);
@@ -106,7 +104,7 @@ public class guiFunctionalitySteps {
 
 //    @Then("^a new profile should be available$")
     @Then("^\"([^\"]*)\" profile/s should be available$")
-    public void aNewProfileShouldBeAvailable(int numberOfProfiles) throws Throwable {
+    public void aNewProfileShouldBeAvailable(int numberOfProfiles) {
         waitForDom(driver);
         waitForObject(driver, "selectUserTitle", true);
         JavascriptExecutor executor = (JavascriptExecutor)driver;
@@ -118,7 +116,7 @@ public class guiFunctionalitySteps {
     }
 
     @Given("^a existing profile$")
-    public void aExistingProfile() throws Throwable {
+    public void aExistingProfile() {
         aNewProfileCalledIsCreated("Baby");
     }
 
@@ -127,90 +125,123 @@ public class guiFunctionalitySteps {
         clickOnObject(driver, "//*[@class='ng-star-inserted item md in-list ion-focusable hydrated']", false);
     }
 
-    @When("^user types in his pin under \"([^\"]*)\"$")
-    public void userTypesInHisPinUnder(String arg1) throws Throwable {
+    @When("^user navigates to user account page")
+    public void userNavigatesToAccountPage() {
         clickOnObject(driver, "tab-button-settings", true);
         clickOnObject(driver, "goToUserAccount", true);
-        clickOnObject(driver, "logAccountOut", true);
         // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        waitForObject(driver, "settingPageTitle", true);
+        Assert.assertEquals(URL + "user-account", driver.getCurrentUrl());
+//        throw new PendingException();
     }
 
     @When("^clicks on Logout in the next page$")
-    public void clicksOnLogoutInTheNextPage() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void clicksOnLogoutInTheNextPage() {
+        waitForDom(driver);
+        waitForObject(driver, "userPageTitle", true);
+        clickOnObject(driver, "//button[@class='mat-button mat-button-base mat-primary ng-star-inserted']/span[text()='Sign out']/parent::*", false);
+        assert true;
     }
 
     @When("^User Logs out$")
-    public void userLogsOut() throws Throwable {
+    public void userLogsOut() {
         // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
 
     @When("^types types in his credentials on Login Page$")
-    public void typesTypesInHisCredentialsOnLoginPage() throws Throwable {
+    public void typesTypesInHisCredentialsOnLoginPage() {
         // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
 
     @Given("^a user has an registered account$")
-    public void hasARegisteredAccount() throws Throwable {
+    public void hasARegisteredAccount() {
         assert true;
     }
 
     @Given("^is on the start page$")
-    public void isOnStartPage() throws Throwable {
+    public void isOnStartPage() {
         driver.navigate().to(URL);
         assert true;
     }
 
     @When("^User logs in$")
-    public void userLogsIn() throws Throwable {
+    public void userLogsIn() {
         aLoggedInUser();
     }
 
     @Then("^he should logged in$")
-    public void heShouldLoggedIn() throws Throwable {
+    public void heShouldLoggedIn()  {
         checkIfObjectExistis(driver, "createUserHeader");
-        assert driver.getCurrentUrl().equals("http://localhost:8100/select-user-profile");
+        assert driver.getCurrentUrl().equals(URL + "select-user-profile");
         driver.quit();
     }
 
-    @When("^User adds new Storie Titles$")
-    public void userAddsNewStorieTitles() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @When("^User adds new Story Title$")
+    public void userAddsNewStoryTitle() throws InterruptedException {
+        waitForDom(driver);
+        clickOnObject(driver, "storyMenuAddNewStory", true);
+        TimeUnit.MILLISECONDS.sleep(250);
+        clickOnObject(driver, "//ion-card[@class=\"ng-star-inserted sc-ion-card-md-h sc-ion-card-md-s md hydrated\"]", false);
+        TimeUnit.MILLISECONDS.sleep(250);
+        clickOnObject(driver, "//button[@class=\"alert-button ion-focusable ion-activatable sc-ion-alert-md\"]", false);
+        assert true;
     }
 
     @Then("^the new Stories should be available in the \"([^\"]*)\" section$")
-    public void theNewStoriesShouldBeAvailableInTheSection(String arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void theNewStoriesShouldBeAvailableInTheSection(String arg1) throws InterruptedException {
+        clickOnObject(driver, "tab-button-story-menu", true);
+        TimeUnit.MILLISECONDS.sleep(250);
+        waitForObject(driver, "//ion-card[@class=\"ng-star-inserted sc-ion-card-md-h sc-ion-card-md-s md hydrated\"]", false);
+        assert true;
+    }
+
+    @Given("^a added story$")
+    public void aAddedStory() throws InterruptedException  {
+        this.userAddsNewStoryTitle();
     }
 
     @When("^User changes Font-Size to (\\d+)px$")
-    public void userChangesFontSizeToPx(int arg1) throws Throwable {
+    public void userChangesFontSizeToPx(int arg1) throws InterruptedException{
+        waitForDom(driver);
+        clickOnObject(driver, "tab-button-settings", true);
+        clickOnObject(driver, "settingsFontSize", true);
+        TimeUnit.SECONDS.sleep(1);
+        waitForObject(driver, "alert-input-2-1", true);
+        clickOnObject(driver, "alert-input-2-1", true);
+        clickOnObject(driver, "//span[@class=\"alert-button-inner sc-ion-alert-md\" and contains(text(), \"Ok\")]", false);
         // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        assert true;
     }
 
     @Then("^Font size is (\\d+)px$")
     public void fontSizeIsPx(int arg1) throws Throwable {
+        waitForDom(driver);
+        clickOnObject(driver, "tab-button-story-menu", true);
+        waitForDom(driver);
+        clickOnObject(driver, "ion-button[@class=\"ion-color ion-color-success md button button-solid ion-activatable ion-focusable hydrated\"]", false);
         // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
 
     @When("^User changes the Language from \"([^\"]*)\" to \"([^\"]*)\"$")
-    public void userChangesTheLanguageFromTo(String arg1, String arg2) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void userChangesTheLanguageFromTo(String arg1, String arg2) throws InterruptedException {
+        waitForDom(driver);
+        clickOnObject(driver, "tab-button-settings", true);
+        clickOnObject(driver, "settingsChangeLanguage", true);
+        TimeUnit.SECONDS.sleep(1);
+        clickOnObject(driver, "//div[@class=\"alert-radio-label sc-ion-alert-md\" and contains(text(), \"English\")]/parent::div/parent::*", false);
+        clickOnObject(driver, "//span[@class=\"alert-button-inner sc-ion-alert-md\" and contains(text(), \"OK\")]", false);
+        assert true;
     }
 
     @Then("^the Application should be in English$")
-    public void theApplicationShouldBeInEnglish() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void theApplicationShouldBeInEnglish() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(1);
+        waitForObject(driver, "ion-sel-0-lbl", true );
+        WebElement element = driver.findElement(By.id("ion-sel-0-lbl"));
+        Assert.assertEquals("Language", element.getText());
     }
 
     @When("^user typed in hin pin under \"([^\"]*)\"$")
@@ -221,8 +252,10 @@ public class guiFunctionalitySteps {
 
     @When("^clicks on delete account$")
     public void clicksOnDeleteAccount() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        waitForDom(driver);
+        waitForObject(driver, "userPageTitle", true);
+        clickOnObject(driver, "//button[@class='mat-button mat-button-base mat-warn ng-star-inserted']/span[text()='Delete account']/parent::*", false);
+        assert true;
     }
 
     @Then("^he should not able to login again$")
